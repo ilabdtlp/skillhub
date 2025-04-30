d4ce56779a6fddc0eda2a0d61e3c31f7efa93d9c
Abdul Wajid <abdulwajidnv@gmail.com>
2025-05-01 01:38:02 +0530

    add favicon and some footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function FooterComponent() {
   return (
-    <footer className="footer sm:footer-horizontal footer-center text-base-300 p-4">
+    <footer className="footer footer-horizontal footer-center text-base-300 hidden p-4 lg:grid">
       <aside>
         <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by iLAB
-          (Innovation Laboratory)
+          © {new Date().getFullYear()} iLAB (Innovation Laboratory). All rights
+          reserved.
         </p>
       </aside>
     </footer>
