160e6441ea9bea7b8383444b3da7e69367fcf9c3
Abdul Wajid <abdulwajidnv@gmail.com>
2025-04-30 23:18:30 +0530

    create dummy checkin, checkout, and occupants page

---
+++ app/checkout/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function CheckoutPage() {
+  return (
+    <div className="flex h-svh flex-col items-center justify-center gap-4 text-center">
+      <h1 className="text-2xl font-bold">Check out of Skill Hub</h1>
+      <p>This page is under construction. 🚧</p>
+      <Link href="/" className="text-blue-600 underline">
+        Go to home page
+      </Link>
+    </div>
+  );
+}
---
+++ app/occupants/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function OccupantsPage() {
+  return (
+    <div className="flex h-svh flex-col items-center justify-center gap-4 text-center">
+      <h1 className="text-2xl font-bold">See who is inside Skill Hub</h1>
+      <p>This page is under construction. 🚧</p>
+      <Link href="/" className="text-blue-600 underline">
+        Go to home page
+      </Link>
+    </div>
+  );
+}
