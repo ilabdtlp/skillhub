a04a65a954b7f4c2240b6c77935b0ee9f51a5940
Abdul Wajid <abdulwajidnv@gmail.com>
2025-06-29 12:02:06 +0530

    add calendar page

---
+++ app/calendar/page.tsx
@@ -0,0 +1,10 @@
+export default function CalendarPage() {
+  return (
+    <main className="flex flex-1 px-3">
+      <iframe
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FKolkata&title=What's%20happening%20%7C%20Skill%20Hub%20by%20iLAB&showPrint=0&showTz=0&mode=AGENDA&src=Y2U0NWRmMWRkMTkxM2ZhOTk0MWM5Y2FlYmIzYWYzY2RmYzZmNzVmOGI2Mjg1Zjk5ZmFhZTI2ZDI4NjhiNWM1YUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23e4c441"
+        className="border-base-300 w-full rounded-xl border"
+      ></iframe>
+    </main>
+  );
+}
